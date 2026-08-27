15a105f2797fc1d15ffc483b5b3ec68278fa837e
SoumyeChauhan26B3039 <soumye.chauhan1908@gmail.com>
2026-08-27 11:29:19 +0530

    modified the main code

--- Task 1.py
+++ Task 1.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import cv2
+img = cv2.imread("c:\\Users\\Dell\\Desktop\\Soumye Chauhan 26-B3-39\\1.png", 0)
+img = cv2.resize(img, (400, 400))
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
